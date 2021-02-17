031b3d1946b9e68921a6a3e1550dd0d0838945ad
Greg <gfernandes7167@gmail.com>
2021-02-17 04:18:32 -0600

    Add files via upload

---
+++ src/Item.java
@@ -0,0 +1,138 @@
+package com.example.android.MaizeMexicanGrill;
+
+/**
+ * Represents an menu item in our application
+ */
+public class Item {
+
+    public static Item[] ITEMS = new Item[] {
+            new Item(R.string.starters, R.string.guacamole,R.drawable.guacamole, R.string.guacamole_desc,R.string.sixteen_dollar, new int[] {R.drawable.vegan, R.drawable.gluten_free}),
+            new Item(R.string.starters, R.string.queso_fundido, R.drawable.queso_fundido, R.string.queso_fundido_desc, R.string.nine_dollar),
+            new Item(R.string.starters,R.string.nachos, R.drawable.nachos, R.string.nachos_desc, R.string.eleven_dollar),
+            new Item(R.string.entrees, R.string.carne_asada,R.drawable.carne_asada, R.string.carne_asada_desc,R.string.sixteen_dollar,  new int[] {R.drawable.dairy_free}),
+            new Item(R.string.entrees, R.string.tampiquena, R.drawable.tampiquena, R.string.tampiquena_desc, R.string.sixteen_dollar),
+            new Item(R.string.entrees, R.string.mega_tiacoyo, R.drawable.mega_tiacoyo, R.string.mega_desc, R.string.fiften_dollar),
+            new Item(R.string.entrees, R.string.fajitas,R.drawable.fajitas_de_pollo_o_res, R.string.fajitas_desc, R.string.fiften_dollar),
+            new Item(R.string.entrees, R.string.milanesa, R.drawable.milanesas, R.string.milanesa_desc, R.string.fiften_dollar, new int[] {R.drawable.dairy_free}),
+            new Item(R.string.entrees, R.string.pechuga_en_salsa, R.drawable.pechugas_en_salsa_chipotle, R.string.pechuaga_en_salsa_desc, R.string.fiften_dollar),
+            new Item(R.string.entrees, R.string.pechuga_plancha,R.drawable.pechuga_a_la_plancha, R.string.pechuga_plancha_desc, R.string.fourteen_dollar, new int[] {R.drawable.dairy_free}),
+            new Item(R.string.entrees, R.string.enchiladas, R.drawable.enchiladas_en_salsa_verde, R.string.enchiladas_desc, R.string.twelve_dollar),
+            new Item(R.string.entrees, R.string.flautas, R.drawable.flautas, R.string.flautas_desc, R.string.eleven_dollar),
+            new Item(R.string.tacos, R.string.taco_americano,R.drawable.taco_americano, R.string.taco_americano_desc, R.string.two_nintey_nine_dollar),
+            new Item(R.string.tacos, R.string.taco_mexica, R.drawable.taco_mexica, R.string.taco_mexica_desc, R.string.three_twenty_five_dollar),
+            new Item(R.string.tacos, R.string.taco_de_lomo, R.drawable.taco_de_lomo, R.string.taco_de_lomo_desc, R.string.three_forty_five_dollar, new int[] {R.drawable.dairy_free}),
+            new Item(R.string.tacos, R.string.taco_de_pescado,R.drawable.taco_de_pescado, R.string.taco_de_pescado_desc, R.string.three_twenty_five_dollar, new int[] {R.drawable.dairy_free}),
+            new Item(R.string.tacos, R.string.taco_campesino, R.drawable.taco_campesino, R.string.taco_campesino_desc, R.string.five_twenty_five_dollar, new int[] {R.drawable.dairy_free}),
+            new Item(R.string.tacos, R.string.taco_veggie, R.drawable.taco_veggie, R.string.taco_veggie_desc, R.string.two_fifty_dollar, new int[] {R.drawable.vegan, R.drawable.gluten_free}),
+            new Item(R.string.cocktails, R.string.fresh_lime_margarita, R.drawable.fresh_lime_margarita, R.string.fresh_lime_margarita_desc, R.string.six_fifty_dollar, new int[] {R.drawable.dairy_free}),
+            new Item(R.string.cocktails, R.string.paloma, R.drawable.paloma, R.string.paloma_desc, R.string.six_fifty_dollar, new int[] {R.drawable.dairy_free}),
+            new Item(R.string.cocktails, R.string.russo_mexicano, R.drawable.russo_mexicano, R.string.russo_mexicano_desc, R.string.seven_dollar),
+            new Item(R.string.cocktails, R.string.seasonal_margarita_avacado, R.drawable.seasonal_margarita___avacado, R.string.seasonal_margarita_avacado_desc, R.string.eleven_dollar, new int[] {R.drawable.dairy_free, R.drawable.vegan}),
+            new Item(R.string.cocktails, R.string.seasonal_margarita_blackberry_mint, R.drawable.seasonal_margarita___blackberry_mint, R.string.seasonal_margarita_blackberry_mint_desc, R.string.eight_fifty_dollar, new int[] {R.drawable.dairy_free, R.drawable.vegan}),
+            new Item(R.string.cocktails, R.string.mango_habanero, R.drawable.mango_habanero, R.string.mango_habanero_desc, R.string.seven_fifty_dollar, new int[] {R.drawable.dairy_free, R.drawable.vegan}),
+            new Item(R.string.cocktails, R.string.seasonal_margarita_watermelon, R.drawable.watermelon_margarita, R.string.seasonal_margarita_watermelon_desc, R.string.six_fifty_dollar, new int[] {R.drawable.dairy_free, R.drawable.vegan}),
+            new Item(R.string.desserts, R.string.flan, R.drawable.flan, R.string.flan_desc, R.string.five_dollar),
+            new Item(R.string.desserts, R.string.paletas, R.drawable.paletas, R.string.paletas_desc, R.string.three_dollar, new int[] {R.drawable.dairy_free, R.drawable.vegan}),
+    };
+
+
+    public static Item getItem(int id) {
+        for (Item item : ITEMS) {
+            if (item.getId() == id) {
+                return item;
+            }
+        }
+        return null;
+    }
+
+    private final int mtype;
+    private final int mName;
+    private final int mFoodImage;
+    private final int mPrice;
+    private final int mDescription;
+    private final String mNutrition;
+    private final int[] mFoodLabels;
+
+
+    Item (int type, int name, int foodImage, int desc, int price) {
+        mtype = type;
+        mName = name;
+        mFoodImage = foodImage;
+        mPrice = price;
+        mDescription = desc;
+        mNutrition = "Nutritional Info:";
+        mFoodLabels = null;
+    }
+
+
+    Item (int type, int name, int foodImage, int desc, int price, int[] labels) {
+        mtype = type;
+        mName = name;
+        mFoodImage = foodImage;
+        mPrice = price;
+        mDescription = desc;
+        mNutrition = "Nutritional Info:";
+        mFoodLabels = labels;
+    }
+
+    Item (int type, int name, int foodImage, int desc, int price, String nutr, int[] labels) {
+        mtype = type;
+        mName = name;
+        mFoodImage = foodImage;
+        mPrice = price;
+        mDescription = desc;
+        mNutrition = nutr;
+        mFoodLabels = labels;
+    }
+
+    Item(Item i) {
+        mtype = i.mtype;
+        mName = i.mName;
+        mFoodImage = i.mFoodImage;
+        mPrice = i.mPrice;
+        mDescription = i.mDescription;
+        mNutrition = i.mNutrition;
+        mFoodLabels = i.mFoodLabels;
+    }
+
+    public int getMtype() { return mtype; }
+
+    public int getId() {
+        return mName + mFoodImage;
+    }
+
+    public int getName() {
+        return mName;
+    }
+
+    public int getPhoto() {
+        return mFoodImage;
+    }
+
+    public int getThumbnail() {
+        return mFoodImage;
+    }
+
+    public int getDescription() {
+        return mDescription;
+    }
+
+    public int getPrice() {
+        return mPrice;
+    }
+
+    public String getNutrition() {
+        return mNutrition;
+    }
+
+    public int[] getFoodLabels() {
+        return mFoodLabels;
+    }
+
+    public int getFoodLabelCount() {
+        if (mFoodLabels == null) {
+            return 0;
+        }
+        return mFoodLabels.length;
+    }
+}
---
+++ src/MainActivity.java
@@ -0,0 +1,297 @@
+
+package com.example.android.MaizeMexicanGrill;
+
+import android.annotation.SuppressLint;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ScrollView;
+import android.widget.SearchView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
+
+import com.squareup.picasso.Picasso;
+
+/**
+ * Our main Activity in this sample. Displays a grid of items which an image and title. When the
+ * user clicks on an item, {@link DetailActivity} is launched, using the Activity Scene Transitions
+ * framework to animatedly do so.
+ */
+public class MainActivity extends AppCompatActivity {
+    private static String food_label;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid);
+
+        // Setup the GridView and set the adapter
+        GridView grid = findViewById(R.id.grid_starter);
+        GridView grid2 = findViewById(R.id.grid_entree);
+        GridView grid3 = findViewById(R.id.grid_taco);
+        GridView grid4 = findViewById(R.id.grid_cocktail);
+        GridView grid5 = findViewById(R.id.grid_dessert);
+        grid.setOnItemClickListener(mOnItemClickListener);
+        grid2.setOnItemClickListener(mOnItemClickListener);
+        grid3.setOnItemClickListener(mOnItemClickListener);
+        grid4.setOnItemClickListener(mOnItemClickListener);
+        grid5.setOnItemClickListener(mOnItemClickListener);
+        GridAdapter adapter = new GridAdapter(R.string.starters);
+        GridAdapter adapter2 = new GridAdapter(R.string.entrees);
+        GridAdapter adapter3 = new GridAdapter(R.string.tacos);
+        GridAdapter adapter4 = new GridAdapter(R.string.cocktails);
+        GridAdapter adapter5 = new GridAdapter(R.string.desserts);
+        grid.setAdapter(adapter);
+        grid2.setAdapter(adapter2);
+        grid3.setAdapter(adapter3);
+        grid4.setAdapter(adapter4);
+        grid5.setAdapter(adapter5);
+
+        // Menu Nav Bar
+        ScrollView scroll_view = findViewById(R.id.scroll_view);
+        @SuppressLint("WrongViewCast") final Button starters_button = findViewById(R.id.starters_button);
+        starters_button.setOnClickListener(v -> {
+            scroll_view.smoothScrollTo(0, ((findViewById(R.id.starter_head)).getTop()));
+        });
+        @SuppressLint("WrongViewCast") final Button entrees_button = findViewById(R.id.entrees_button);
+        entrees_button.setOnClickListener(v -> {
+            scroll_view.smoothScrollTo(0, ((findViewById(R.id.entree_head)).getTop()));
+        });
+        @SuppressLint("WrongViewCast") final Button tacos_button = findViewById(R.id.tacos_button);
+        tacos_button.setOnClickListener(v -> {
+            scroll_view.smoothScrollTo(0, ((findViewById(R.id.taco_head)).getTop()));
+        });
+        @SuppressLint("WrongViewCast") final Button cocktails_button = findViewById(R.id.cocktails_button);
+        cocktails_button.setOnClickListener(v -> {
+            scroll_view.smoothScrollTo(0, ((findViewById(R.id.cocktail_head)).getTop()));
+        });
+        @SuppressLint("WrongViewCast") final Button desserts_button = findViewById(R.id.desserts_button);
+        desserts_button.setOnClickListener(v -> {
+            scroll_view.smoothScrollTo(0, ((findViewById(R.id.dessert_head)).getTop()));
+        });
+
+        // Food Labels Drop-Down Filter
+        Spinner food_labels_spinner = findViewById(R.id.food_labels_spinner);
+        ArrayAdapter<CharSequence> spinner_adapter = ArrayAdapter.createFromResource(this,
+                R.array.food_labels, android.R.layout.simple_spinner_dropdown_item);
+        food_labels_spinner.setAdapter(spinner_adapter);
+        food_labels_spinner.setOnItemSelectedListener(mOnItemSelectedListener);
+    }
+
+    private final AdapterView.OnItemClickListener mOnItemClickListener
+            = new AdapterView.OnItemClickListener() {
+
+        /**
+         * Called when an item in the {@link android.widget.GridView} is clicked. Here will launch
+         * the {@link DetailActivity}, using the Scene Transition animation functionality.
+         */
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+            Item item = (Item) adapterView.getItemAtPosition(position);
+
+            // Construct an Intent as normal
+            Intent intent = new Intent(MainActivity.this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_PARAM_ID, item.getId());
+
+            // BEGIN_INCLUDE(start_activity)
+            /*
+             * Now create an {@link android.app.ActivityOptions} instance using the
+             * {@link ActivityOptionsCompat#makeSceneTransitionAnimation(Activity, Pair[])} factory
+             * method.
+             */
+            @SuppressWarnings("unchecked")
+            ActivityOptionsCompat activityOptions = ActivityOptionsCompat.makeSceneTransitionAnimation(
+                    MainActivity.this,
+
+                    // Now we provide a list of Pair items which contain the view we can transitioning
+                    // from, and the name of the view it is transitioning to, in the launched activity
+                    new Pair<>(view.findViewById(R.id.food_image),
+                            DetailActivity.VIEW_NAME_HEADER_IMAGE),
+                    new Pair<>(view.findViewById(R.id.food_name),
+                            DetailActivity.VIEW_NAME_HEADER_TITLE));
+
+            // Now we can start the Activity, providing the activity options as a bundle
+            ActivityCompat.startActivity(MainActivity.this, intent, activityOptions.toBundle());
+            // END_INCLUDE(start_activity)
+        }
+    };
+
+    /**
+     * {@link android.widget.BaseAdapter} which displays items.
+     */
+    private class GridAdapter extends BaseAdapter {
+
+        int type;
+        public GridAdapter(int foodtype) {
+            type = foodtype;
+        }
+
+        @Override
+        public int getCount() {
+            int total_type = 0;
+            for (int i = 0; i < Item.ITEMS.length; i++) {
+                if (type == Item.ITEMS[i].getMtype()) {
+                    total_type++;
+                }
+            }
+            return total_type;
+        }
+
+        @Override
+        public Item getItem(int position) {
+            int i = 0;
+            while (type != Item.ITEMS[i].getMtype()) {
+                i++;;
+            }
+            return Item.ITEMS[position+i];
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return getItem(position).getId();
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+            if (view == null) {
+                view = getLayoutInflater().inflate(R.layout.grid_item, viewGroup, false);
+            }
+
+            final Item item = getItem(position);
+            if (item.getMtype() == type) {
+                // Load the thumbnail image
+                ImageView image = view.findViewById(R.id.food_image);
+                Picasso.with(image.getContext()).load(item.getThumbnail()).into(image);
+
+                if (item.getFoodLabels() != null) {
+                    ImageView[] labels = new ImageView[item.getFoodLabelCount()];
+                    for (int i = 0; i < item.getFoodLabelCount(); i++) {
+                        if (i == 0) {
+                            labels[i] = view.findViewById(R.id.food_label_1);
+                        } else {
+                            labels[i] = view.findViewById(R.id.food_label_2);
+
+                        }
+                        Picasso.with(labels[i].getContext()).load(item.getFoodLabels()[i]).into(labels[i]);
+                    }
+                }
+
+                // Set the TextView's contents
+                TextView name = view.findViewById(R.id.food_name);
+                name.setText(item.getName());
+
+                TextView price = view.findViewById(R.id.price);
+                price.setText(item.getPrice());
+
+
+            }
+            return view;
+
+        }
+    }
+    private final AdapterView.OnItemSelectedListener mOnItemSelectedListener
+            = new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+            String[] food_labels_arr = getResources().getStringArray(R.array.food_labels);
+            food_label = food_labels_arr[pos];
+            // user still on "filter" text option
+            if (pos == 0) {
+                food_label = "";
+                return;
+            }
+            // search bar blank
+            SearchActivity.setQuery("");
+            Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+            startActivity(intent);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    };
+    public static String getFoodLabel() {
+        // Names on UI and in drawable files differ
+        switch(food_label) {
+            case "Vegan":
+            case "vegan":
+                food_label = "vegan";
+                break;
+            case "Gluten Free":
+            case "gluten_free":
+                food_label = "gluten_free";
+                break;
+            case "Dairy Free":
+            case "dairy_free":
+                food_label = "dairy_free";
+                break;
+            default:
+                Log.i("m","executed");
+                food_label = "";
+        }
+
+        return food_label;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager =
+                (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView =
+                (SearchView) menu.findItem(R.id.search).getActionView();
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(getComponentName()));
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.i("Testing Query", "Success");
+                callSearch(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                //              if (searchView.isExpanded() && TextUtils.isEmpty(newText)) {
+                //Log.i("Testing Query", "Success");
+                //callSearch(newText);
+                //
+                return true;
+            }
+
+            public void callSearch(String query) {
+                //Do searching
+                Log.i("Home search Query: ", query);
+                SearchActivity.setQuery(query);
+                Intent intent = new Intent(MainActivity.this, SearchActivity.class);
+                startActivity(intent);
+                // Transition to SearchActivity
+            }
+
+        });
+
+        return true;
+    }
+}
